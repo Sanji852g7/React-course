c9b2d79221cee7e6e3900046534b9c0b9d5c7751
Sanji Gurung <sanji.gurung@hotmail.com>
2026-04-02 23:11:42 +0100

    proper react set up

---
+++ 5-react-setup/chatbot-project/src/components/ChatMessages.jsx
@@ -0,0 +1,38 @@
+import { useRef, useEffect } from 'react'
+import { ChatMessage } from '../components/ChatMessage';
+import './ChatMessages.css';
+
+function ChatMessages({ chatMessages }) {
+  const chatMessagesRef = useAutoScroll([chatMessages]);
+
+  function useAutoScroll(dependencies){
+    const containerRef = useRef(null);
+  
+    useEffect(()=> {
+      const containerElem = containerRef.current;
+      if(containerElem) {
+        containerElem.scrollTop = containerElem.scrollHeight;
+      }
+    }, [dependencies]);
+  
+    return containerRef;
+  }
+
+  return  (
+    <div className="chat-messages-container"
+      ref={chatMessagesRef}
+    >
+      {chatMessages.map((chatMessage) => {
+        return (
+          <ChatMessage 
+            message = {chatMessage.message}
+            sender = {chatMessage.sender}
+            key = {chatMessage.id}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default ChatMessages;
