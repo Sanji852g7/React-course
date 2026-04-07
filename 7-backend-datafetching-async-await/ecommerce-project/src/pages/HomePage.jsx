7c90270927b8fec1ab22283b333024756aa886b0
Sanji Gurung <sanji.gurung@hotmail.com>
2026-04-07 21:38:34 +0100

    use data from the backend to generate the html

--- 7-backend-datafetching-async-await/ecommerce-project/src/pages/HomePage.jsx
+++ 7-backend-datafetching-async-await/ecommerce-project/src/pages/HomePage.jsx
@@ -1,15 +1,20 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Header } from '../components/Header';
 import CheckmarkIcon from '../assets/images/icons/checkmark.png';
-import { products } from '../../starting-code/data/products';
 import { Link } from 'react-router';
 import './HomePage.css';
 
 export function HomePage() {
-  axios.get('http://localhost:3000/api/products')
-    .then((response)=> {
-      console.log(response.data);
-    });
+  const [products, setProducts ] = useState([]);
+
+  useEffect(()=> {
+    axios.get('http://localhost:3000/api/products')
+      .then((response)=> {
+        setProducts(response.data);
+      });
+  }, []);
+  
 
   return (
     <>
