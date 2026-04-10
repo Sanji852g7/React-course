48b84df4bee08ae14665b8caf7355e3459535d08
Sanji Gurung <sanji.gurung@hotmail.com>
2026-04-10 17:39:39 +0100

    create add to cart feature

--- 8-datamutation-requests/ecommerce-project/src/pages/home/HomePage.jsx
+++ 8-datamutation-requests/ecommerce-project/src/pages/home/HomePage.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router';
 import { ProductsGrid } from './ProductsGrid';
 import './HomePage.css';
 
-export function HomePage({cart}) {
+export function HomePage({ cart, loadCart }) {
   const [products, setProducts ] = useState([]);
 
   useEffect(()=> {
@@ -24,7 +24,7 @@ export function HomePage({cart}) {
       <Link rel="icon" type="image/svg+xml" to="home-favicon.png" />
       <Header cart={cart} />
       <div className="home-page">
-        <ProductsGrid products={products}/>
+        <ProductsGrid products={products} loadCart={loadCart}/>
       </div>
     </>
   );
