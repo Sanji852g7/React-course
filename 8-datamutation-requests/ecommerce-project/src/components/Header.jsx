6b0a88494e841971feeaa4e53a936ae6fbfad74c
Sanji Gurung <sanji.gurung@hotmail.com>
2026-04-13 23:09:08 +0100

    make the search bar at the top of the home page work

--- 8-datamutation-requests/ecommerce-project/src/components/Header.jsx
+++ 8-datamutation-requests/ecommerce-project/src/components/Header.jsx
@@ -3,11 +3,26 @@ import CartIcon from '../assets/images/icons/cart-icon.png';
 import SearchIcon from '../assets/images/icons/search-icon.png';
 import LogoWhite from '../assets/images/logo-white.png';
 import MobileLogoWhite from '../assets/images/mobile-logo-white.png';
-import { NavLink } from 'react-router';
+import { NavLink, useNavigate, useSearchParams } from 'react-router';
+import { useState } from 'react';
 
 export function Header({cart=[]}){
-  let totalQuantity = 0;
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
 
+  const searchText = searchParams.get('search');
+
+  const [search, setSearch] = useState(searchText || '');
+
+  const updateSearchInput = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const searchProducts = () => {
+    navigate(`/?search=${search}`);
+  };
+
+  let totalQuantity = 0;
   cart.forEach((cartItem) => {
     totalQuantity += cartItem.quantity;
   });
@@ -24,9 +39,9 @@ export function Header({cart=[]}){
         </div>
 
         <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
+          <input className="search-bar" type="text" placeholder="Search" value={search} onChange={updateSearchInput} />
 
-          <button className="search-button">
+          <button className="search-button" onClick={searchProducts}>
             <img className="search-icon" src={SearchIcon} />
           </button>
         </div>
