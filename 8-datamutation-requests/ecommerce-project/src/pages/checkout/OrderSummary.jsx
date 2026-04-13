252b7710025c0ef992f42c20a1b97a0930fa93d4
Sanji Gurung <sanji.gurung@hotmail.com>
2026-04-13 22:44:56 +0100

    add quantity input feature using controlled input and keyboard events

--- 8-datamutation-requests/ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ 8-datamutation-requests/ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -23,7 +23,7 @@ export function OrderSummary({ cart, deliveryOptions, loadCart }) {
 
             <div className="cart-item-details-grid">
 
-              <CartItemDetails cartItem={cartItem} deleteCartItem={deleteCartItem} />
+              <CartItemDetails cartItem={cartItem} deleteCartItem={deleteCartItem} loadCart={loadCart}/>
 
               <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions} loadCart={loadCart} deleteCartItem={deleteCartItem}/>
             </div>
