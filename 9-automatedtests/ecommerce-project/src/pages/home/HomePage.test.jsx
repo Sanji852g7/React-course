ea4e8b268208f04350bb39697e9811c066e0b318
Sanji Gurung <sanji.gurung@hotmail.com>
2026-04-17 00:05:26 +0100

    create a test if the add to cart buttons work

--- 9-automatedtests/ecommerce-project/src/pages/home/HomePage.test.jsx
+++ 9-automatedtests/ecommerce-project/src/pages/home/HomePage.test.jsx
@@ -1,17 +1,19 @@
 import axios from "axios";
 import { expect, it, describe, vi, beforeEach } from 'vitest';
 import { render, screen, within } from '@testing-library/react';
-//import userEvent from '@testing-library/user-event';
 import { HomePage } from "./HomePage";
 import { MemoryRouter } from "react-router";;
+import userEvent from '@testing-library/user-event';
 
 vi.mock('axios');
 
 describe('HomePage component', ()=> {
   let loadCart;
+  let user;
 
   beforeEach(()=>{
     loadCart = vi.fn();
+    user = userEvent.setup();
 
     axios.get.mockImplementation(async (urlPath)=> {
       if (urlPath === '/api/products'){
@@ -60,5 +62,33 @@ describe('HomePage component', ()=> {
     expect(
       within(productContainer[1]).getByText('Intermediate Size Basketball')
       ).toBeInTheDocument();
+  });
+
+  //exercise 9g
+  it('adds a product to the cart', async () => {
+    render(
+      <MemoryRouter>
+        <HomePage cart={[]} loadCart={loadCart} />
+      </MemoryRouter>
+    );
+    const productContainers = await screen.findAllByTestId('product-container');
+
+    const addToCartButton1 = within(productContainers[0])
+      .getByTestId('add-to-cart-button');
+    await user.click(addToCartButton1);
+
+    const addToCartButton2 = within(productContainers[1])
+      .getByTestId('add-to-cart-button');
+    await user.click(addToCartButton2);
+
+    expect(axios.post).toHaveBeenNthCalledWith(1, '/api/cart-items', {
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 1
+    });
+    expect(axios.post).toHaveBeenNthCalledWith(2, '/api/cart-items', {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1
+    });
+    expect(loadCart).toHaveBeenCalledTimes(2);
   });
 });
